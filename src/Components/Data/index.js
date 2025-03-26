afb8bd9a3544cdb13783254d63cc6cf56c523efe
ArunDhoundiyal <151505192+ArunDhoundiyal@users.noreply.github.com>
2025-03-26 13:02:39 +0530

    Update index.js

--- src/Components/Data/index.js
+++ src/Components/Data/index.js
@@ -6,18 +6,65 @@ export const navItemArray =[
     {id:5,navItemName:'Contact Us'}
 ]
 export const allServicesDataArray = [
-    {id:1, serviceName:'Video Production & Editing', logoImg:'https://res.cloudinary.com/da52fiag8/image/upload/v1742711277/video-camera_9695298_qshxwb.png'},
-    {id:2, serviceName:'Documentary Shoots', logoImg:'https://res.cloudinary.com/da52fiag8/image/upload/v1742711277/professional-movie-camera_11363459_dp4qyj.png'},
-    {id:3, serviceName:'Short Videos', logoImg:'https://res.cloudinary.com/da52fiag8/image/upload/v1742711276/video-player_3112587_uryhzq.png'},
-    {id:4, serviceName:'Animated Videos', logoImg:'https://res.cloudinary.com/da52fiag8/image/upload/v1742711276/movie_17994233_2_sykjca.png'},
-    {id:5, serviceName:'Tutorial Videos', logoImg:'https://res.cloudinary.com/da52fiag8/image/upload/v1742711277/virtual-reality_3612125_j0dpos.png'},
-    {id:6, serviceName:'Motion Graphic', logoImg:'https://res.cloudinary.com/da52fiag8/image/upload/v1742711276/motion-graphic_14354676_xms7b3.png'},
-    {id:7, serviceName:'Creative & Branding Solutions', logoImg:'https://res.cloudinary.com/da52fiag8/image/upload/v1742711276/creative_1643316_xormza.png'},
-    {id:8, serviceName:'Logo Design', logoImg:'https://res.cloudinary.com/da52fiag8/image/upload/v1742711277/text-editor_7407930_b6bslh.png'},
-    {id:9, serviceName:'Print Media', logoImg:'https://res.cloudinary.com/da52fiag8/image/upload/v1742711276/news-report_17666177_z5co6t.png'},
-    {id:10, serviceName:'Website Designing', logoImg:'https://res.cloudinary.com/da52fiag8/image/upload/v1742712061/responsive-design_1485297_x8fpxn.png'}
+    {id:1, serviceName:'Video Shoot & Productions', description:"From concept to execution, our video shoot and production services ensure high-quality visuals that bring your vision to life. Whether it's brand commercials, event coverage, corporate films, or social media content, our expert team handles everything—pre-production planning, shooting, and post-production—to create visually striking and engaging videos."},
+    {id:2,serviceName:'Wedding Shoot', description:'Capture the most special moments of your big day with our professional wedding shoot services. From pre-wedding shoots to cinematic wedding films, we create timeless, high-quality visuals that beautifully tell your love story. Our expert team ensures every emotion and detail is preserved, creating memories that last a lifetime.'},
+    {id:3, serviceName:'Graphic Designing', description:'Bring your ideas to life with our creative graphic designing solutions. From eye-catching logos and branding materials to social media graphics and marketing collateral, we craft visually stunning designs that leave a lasting impression.'},
+    {id:4, serviceName:'Web Designing & Development', description:'A strong online presence starts with a well-designed website. Our web design services focus on creating aesthetically appealing, user-friendly, and responsive websites that enhance user engagement and drive conversions. Whether you need a portfolio, e-commerce, or business website, we tailor designs to meet your unique needs.'},
+    {id:5, serviceName:'Motion Graphic', description:'Add dynamic elements to your content with our motion graphics services. From animated explainer videos to engaging social media visuals, we use motion design to communicate complex ideas in a visually compelling and engaging way.'},
+    {id:6, serviceName:'Sketching & Doodle Art', description:"Express creativity with our sketching and doodle art services. Whether it's custom illustrations, character designs, hand-drawn elements, or creative doodles for branding and storytelling, we craft unique and artistic visuals that bring ideas to life in an imaginative way"}
 ]
 
+export const whyChooseUs = [
+    {whyChooseUsData:[
+        {
+            id:1, 
+            name:"Creative Excellence", 
+            img:'https://res.cloudinary.com/da52fiag8/image/upload/v1742895415/inspiration-w_l9md2u.png'
+        },
+        {
+            id:2, 
+            name:"Strategic Storytelling", 
+            img:'https://res.cloudinary.com/da52fiag8/image/upload/v1742895415/storytelling-w_rkhrvy.png'
 
- 
+        },
+        {
+            id:3, 
+            name:"Client-Centric Approach",
+            img:'https://res.cloudinary.com/da52fiag8/image/upload/v1742895415/consumer-centric-w_aulu1g.png' 
+        },
+        {
+            id:4, 
+            name:"Seamless Execution", 
+            img:'https://res.cloudinary.com/da52fiag8/image/upload/v1742895415/simplicity-w_hsgy3k.png'
+        }       
+    ]
+}, 
+{
+    companyDirectorAndOperationHeadData:[
+        {
+            id:1, 
+            name:"Vikas Dhoundiyal (Director)", 
+            description:"At Dezyn Edge, we believe that every brand has a unique story waiting to be told. Our mission is to bring those stories to life through compelling visuals and innovative designs. We are committed to delivering creative excellence, ensuring that each project reflects authenticity, innovation, and impact. Whether you're a startup or an established brand, our team is here to help you stand out and make a lasting impression. Let’s collaborate and turn your vision into a powerful reality"
+        },
+        {
+            id:2, 
+            name:"Prince Dua (Director & Operations Head, Dezyn Edge)",
+            description:"Over the years, we have had the privilege of working with some incredible brands and businesses, helping them craft unique and memorable identities. Our commitment remains the same – to deliver top-notch creative solutions that transform brands and drive impact. We look forward to collaborating with you and bringing your ideas to life with creativity, passion, and precision", 
+        }
+    ]
+}
+    
+];
+
+
+export const OurClientsDataArray = [
+    {id:1, img:'https://res.cloudinary.com/da52fiag8/image/upload/v1742900018/WhatsApp_Image_2025-03-25_at_4.22.49_PM_ogq90i.jpg'}, 
+    {id:2, img:'https://res.cloudinary.com/da52fiag8/image/upload/v1742899764/WhatsApp_Image_2025-03-25_at_4.18.08_PM_nc4tzq.jpg'},
+    {id:3, img:'https://res.cloudinary.com/da52fiag8/image/upload/v1742899622/vbu6ntnb1odgunaoyxbh.jpg'},
+    {id:4, img:'https://res.cloudinary.com/da52fiag8/image/upload/v1742899622/pcocok69wqqeemesefw3.jpg'},
+    {id:5, img:'https://res.cloudinary.com/da52fiag8/image/upload/v1742899621/mtsoy9egyizvzwsn5edq.jpg'}, 
+    {id:6, img:'https://res.cloudinary.com/da52fiag8/image/upload/v1742899621/n0n4etbdneulkchg8beh.jpg'},
+    {id:7, img:`https://res.cloudinary.com/da52fiag8/image/upload/v1742905561/jiukr0g1zp6tpqncjzbq.jpg`}
+]
+
 
